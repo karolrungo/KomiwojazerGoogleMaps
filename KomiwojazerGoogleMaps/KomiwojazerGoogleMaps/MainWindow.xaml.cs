b4b5cf421ab7a2fc8fac3d5325e8de1c59205a60
rungokarol <karolrungo@gmail.com>
2015-03-17 19:33:18 +0100

    drawing route on map works

--- KomiwojazerGoogleMaps/KomiwojazerGoogleMaps/MainWindow.xaml.cs
+++ KomiwojazerGoogleMaps/KomiwojazerGoogleMaps/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GMap.NET;
+using GMap.NET.WindowsPresentation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,27 @@ namespace KomiwojazerGoogleMaps
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Initialize map:
+            gmap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+            //gmap.Position = new PointLatLng(-25.971684, 32.589759);
+            gmap.SetPositionByKeywords("Wołów, Poland");
+
+            PointLatLng start = new PointLatLng(51.110, 17.030);
+            PointLatLng end = new PointLatLng(52.259, 21.020);
+
+            RoutingProvider rp = gmap.MapProvider as RoutingProvider;
+            MapRoute route = rp.GetRoute(start, end, false, false, (int)gmap.Zoom);
+
+            GMapMarker m1 = new GMapMarker(start);
+            GMapMarker m2 = new GMapMarker(end);
+            GMapRoute mRoute = new GMapRoute(route.Points);
+            gmap.Markers.Add(mRoute);
+            gmap.ZoomAndCenterMarkers(null);
         }
     }
 }
